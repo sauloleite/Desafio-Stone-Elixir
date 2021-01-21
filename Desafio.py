80b59d3feb7e3534bd8b284fc7020037abc3a123
sauloleite <sjoldeveloper@gmail.com>
2021-01-21 12:23:58 -0300

    Update
    Changes

--- Desafio.py
+++ Desafio.py
@@ -6,11 +6,11 @@ def func(listcomps, listemails): #"listcomps" é a lista de compras e "listemail
     if(len(listcomps) == 3): #Verificador se a lista contém pelo menos 3 itens/listas
         #Verficador de tamanho das listas "Quantidade de cada item" e "Preço por unidade/peso/pacote de cada item"
         if(len(listcomps[1])!=len(listcomps[2])): #Verifica se o comprimento da lista é diferente
-            if(len(listcomps[1]) < len(listcomps[2])): #Caso a lista de itens seja maior a lista de preço será ajustada
+            if(len(listcomps[1]) < len(listcomps[2])): #Caso a lista de itens seja maior, a lista de preço será ajustada
                 medida = len(listcomps[2]) - len(listcomps[1]) #Esta variável será usada para fazer o ajuste
                 for i in range(0, medida): #Laço de repetição para ajustar a lista de itens
                     listcomps[1].append(0) #Serão acrescentados a zeros até que as listas tenham o mesmo comprimento
-            else: #Caso a lista de preço seja maior a lista de itens será ajustada
+            else: #Caso a lista de preço seja maior, a lista de itens será ajustada
                 medida = len(listcomps[1]) - len(listcomps[2]) #Variável de ajuste
                 for i in range(0, medida): #Laço de repetição para ajustar a lista de preços
                     listcomps[2].append(0) #Serão acrescentados a zeros até que as listas tenham o mesmo comprimento
