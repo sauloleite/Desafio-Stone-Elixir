70e534e505fc326624b545825b3e6f8904e71779
sauloleite <sjoldeveloper@gmail.com>
2021-01-21 16:40:44 -0300

    Final change
    Finished

--- Desafio.py
+++ Desafio.py
@@ -1,55 +1,49 @@
 #Função que irá receber uma lista de compras e uma lista de e-mails. 
 def func(listcomps, listemails): #"listcomps" é a lista de compras e "listemails" a de emails
     s = dict() #Criando dicionário da saída de dados
+    #Tratando dados recebidos
     listemails = sorted(set(list(filter(None,listemails)))) #Ordenando e eliminando emails repeditos e vazios
     
-    if(len(listcomps) == 3): #Verificador se a lista contém pelo menos 3 itens/listas
-        #Verficador de tamanho das listas "Quantidade de cada item" e "Preço por unidade/peso/pacote de cada item"
-        if(len(listcomps[1])!=len(listcomps[2])): #Verifica se o comprimento da lista é diferente
-            if(len(listcomps[1]) < len(listcomps[2])): #Caso a lista de itens seja maior, a lista de preço será ajustada
-                medida = len(listcomps[2]) - len(listcomps[1]) #Esta variável será usada para fazer o ajuste
-                for i in range(0, medida): #Laço de repetição para ajustar a lista de itens
-                    listcomps[1].append(0) #Serão acrescentados a zeros até que as listas tenham o mesmo comprimento
-            else: #Caso a lista de preço seja maior, a lista de itens será ajustada
-                medida = len(listcomps[1]) - len(listcomps[2]) #Variável de ajuste
-                for i in range(0, medida): #Laço de repetição para ajustar a lista de preços
-                    listcomps[2].append(0) #Serão acrescentados a zeros até que as listas tenham o mesmo comprimento
+    #Considerando que a lista de compras terá itens com listas neste formato ['Item', Quantidade, Preço], será realizada uma verificação se todos os itens estão em conformidade
+    invalidos = [] #Variável criada para armazenar itens inválidos
+    for i in range(len(listcomps)): #Varretura de itens inválidos na lista 
+        if len(listcomps[i])<3 or len(listcomps[i])>3: #Verificar caso tenha algum item com uma lista maior ou menor que 3 
+           invalidos.append(listcomps[i]) #Armazena este valor na lista "inválidos"
+    for i in invalidos:
+        listcomps.remove(i) #Removendo itens considerados inválidos
+                      
+    #Verificador de emails inválidos
+    emailserro = [] #Criando lista de emails inválidos p/ removê-los posteriormente
+    for i in range(len(listemails)): 
+        if((len(listemails[i]) < 3 or len(listemails[i]) > 254)): #Verifica se é um email válido
+            emailserro.append(listemails[i]) #Adicionando estes emails a lista de emails inválidos
+        elif(not(listemails[i].count('@'))): #Verifica se é um email válido
+            emailserro.append(listemails[i]) #Adicionando estes emails a lista de emails inválidos
+    for i in emailserro: 
+        listemails.remove(i) #Remoção de emails inválidos da lista de emails
 
-        #Verificador de emails inválidos
-        emailserro = [] #Criando lista de emails inválidos p/ removê-los posteriormente
-        for i in range(0, len(listemails)): 
-            if((len(listemails[i]) < 3 or len(listemails[i]) > 254)): #Verifica se é um email válido
-                emailserro.append(listemails[i]) #Adicionando estes emails a lista de emails inválidos
-            elif(not(listemails[i].count('@'))): #Verifica se é um email válido
-                emailserro.append(listemails[i]) #Adicionando estes emails a lista de emails inválidos
-        for i in emailserro: 
-            listemails.remove(i) #Remoção de emails inválidos da lista de emails
-
-        #Calcular a soma dos valores, multiplicar o preço de cada item por sua quantidade e somar todos os itens
-        somaitens = 0 #Variável criada para armazenar o valor da soma dos itens * preços
-        for i in range(0, len(listcomps[1])): 
-            somaitens = listcomps[1][i]*listcomps[2][i] + somaitens #Soma de itens
+    #Calcular a soma dos valores, multiplicar o preço de cada item por sua quantidade e somar todos os itens
+    somaitens = 0 #Variável criada para armazenar o valor da soma dos itens * preços
+    for i in range(len(listcomps)): 
+        somaitens = listcomps[i][1]*listcomps[i][2] + somaitens #Soma de itens
+    
+    #Ajustes para evitar uma divisão por zero
+    qtdemails = len(listemails) #Variável criada para evitar divisão por zero
+    if(qtdemails==0): #Condição criada para evitar a possibilidade de divisão por 0
+        qtdemails = 1 #Atribuindo o valor de 1 para evitar divisão por zero
         
-        #Ajustes para evitar uma divisão por zero
-        qtdemails = len(listemails) #Variável criada para evitar divisão por zero
-        if(qtdemails==0): #Condição criada para evitar a possibilidade de divisão por 0
-            qtdemails = 1 #Atribuindo o valor de 1 para evitar divisão por zero
-            print("Lista sem emails para realização") #Mensagem para avisar o porquê de um S "Vazio"
-        
-        #Dividir o valor de forma igual entre a quantidade de e-mails
-        vpe = int(somaitens/qtdemails) #Valor por email
+    #Dividir o valor de forma igual entre a quantidade de e-mails
+    vpe = int(somaitens/qtdemails) #Valor por email
 
-        #Verificando se não faltará nenhum centavo
-        verif = somaitens - vpe*qtdemails #Verificador de "falta" para possível redistribuição
-        for j in range(0, len(listemails)): 
-            if(verif == 0): #Condição para verificar se não faltará nenhum centavo
+    #Verificando se não faltará nenhum centavo
+    verif = somaitens - vpe*qtdemails #Verificador de "falta" para possível redistribuição
+    for j in range(len(listemails)): 
+        if(verif == 0): #Condição para verificar se não faltará nenhum centavo
+            s[listemails[j]]=vpe
+        else: #Caso falte algum centavo, será acrescentado aos últimos valores do dicionário.
+            if(j==(qtdemails-verif)): #Verificação do último elemento da lista
+                verif -= 1 #Diminui o valor acrescido do verificador
+                s[listemails[j]]=vpe+1 #Acréscimo do valor de verificação (1)
+            else: #Caso não seja o último, ele segue até chegar a ele e acrescenta o valor 
                 s[listemails[j]]=vpe
-            else: #Caso falte algum centavo, será acrescentado ao(s) último(s) valore(s) do dicionário.
-                if(j==(qtdemails-verif)): #Verificação dos últimos elementos da lista
-                    verif -= 1 #Diminui o valor acrescido do verificador
-                    s[listemails[j]]=vpe+1 #Acréscimo do valor de verificação (1)
-                else: #Enquanto não chega ao último(s), ele segue até chegar a ele(s) e acrescenta o valor 
-                    s[listemails[j]]=vpe
-        return s #retorna o dicionário gerado pela função
-    else:
-        print("A lista de compras deve conter 3 itens/listas.")
+    return s #retorna o dicionário gerado pela função
--- Teste.py
+++ Teste.py
@@ -1,13 +1,15 @@
 '''Teste Desafio Stone Elixir
 Desenvolvido por: Saulo Leite'''
-
+#ATENÇÃO: o arquivo: "Desafio.py" deve estar presente na mesma pasta que esse arquivo para poder executá-lo!
 #Importando o módulo com a função pedida no desafio
 import Desafio 
 
-#Lista de compras para testes
-l1 = [['Ovos', 'Leite', 'Farinha', 'Fermento'], [1, 1, 1, 1], [140, 765, 540, 200]] 
-#Lista de emails para testes
-l2= ['zequinha@gmail.com', 'fulano@hotmail.com', 'lindo123@yahoo.com','teste', 'lindo123@yahoo.com', 'lindo123@yahoo.com','lindo123yahoo.com', 's@', 'z@', '@.', '', '´´', '..']
+#A l1 (lista de compras) foi construída da forma que recebe listas com: ['Item'(str), Quantidade(int), Preço(int)]
+
+#Lista de compras para testes com valores aletórios
+l1 =[['Ovo', 12, 100], ['Leite', 1, 650], [], ['.'], ['Teste 1'], ['Teste 2', 0] ,['Teste 3', 1,2,3,] ,['Farinha', 1, 375], ['Fermento', 1, 230], ['']]
+#Lista de emails para testes com emails aleatórios
+l2= ['stevejobs@icloud.com', 'billgates@microsoft.com', 'linustorvalds@unix.com', 'teste' , 'linustorvalds@unix.com', 'linustorvalds@unix.com','linustorvaldsunix.com', 't@', '@', '@.', ' ', '´´', '..']
 
 #Chamando a função do módulo com os dois parâmetros 
 saida = Desafio.func(l1, l2) #"l1" é a lista de compras e "l2" é a de emails
